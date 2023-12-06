98fb4a812ee34c7865c52db8313436e218ca56a9
jahirocean <jahirocean@gmail.com>
2023-12-06 18:14:52 +0600

    update

--- latticeBoltzmann.py
+++ latticeBoltzmann.py
@@ -8,14 +8,6 @@ Created on Tue Dec  5 23:49:29 2023
 import matplotlib.pyplot as plt
 import numpy as np
 
-"""
-Create Your Own Lattice Boltzmann Simulation (With Python)
-Philip Mocz (2020) Princeton Univeristy, @PMocz
-
-Simulate flow past cylinder
-for an isothermal fluid
-
-"""
 
 
 
